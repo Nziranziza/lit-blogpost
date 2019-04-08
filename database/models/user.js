f2300a42f1e2cbb98fec739cf1bfedcb78af17d2
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-04-08 12:16:20 +0200

    chore: change npm start script for heroku

--- database/models/user.js
+++ database/models/user.js
@@ -1,35 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4
-      },
-      avatar: DataTypes.STRING,
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password: DataTypes.STRING,
-      gender: DataTypes.STRING,
-      userType: DataTypes.STRING,
-      birthDate: DataTypes.DATE,
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false
-      }
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
     },
-    {}
-  );
+    avatar: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    userType: DataTypes.STRING,
+    birthDate: DataTypes.DATE,
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
+
   User.associate = models => {
     // associations can be defined here
     User.hasMany(models.Token, { foreignKey: 'userId' });
